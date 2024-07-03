a8e9f8905a9f15121c2f51ebc135156b0c818c3d
fikratkhatira <fekratkhatira@gmail.com>
2024-07-02 20:45:33 -0700

    js final error fixed

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
-checkBtn.addEventListener('click', palindrome);
-
 function palindrome() {
   if (textInput.value === '') {
     console.warn('Please input a value');
+    result.innerHTML = '';
     return;
   }
 
   const processedStr = textInput.value.replace(/[\W_]/g, '').toLowerCase();
   let isPalindrome = true;
 
-  for (let i = 0; i < Math.floor(processedStr.length / 2); i += 1) { 
+  for (let i = 0; i < Math.floor(processedStr.length / 2); i += 1) {
     if (processedStr[i] !== processedStr[processedStr.length - 1 - i]) {
       isPalindrome = false;
       break;
@@ -21,8 +20,10 @@ function palindrome() {
   }
 
   if (isPalindrome) {
-    result.innerHTML = textInput.value + ' is a palindrome';
+    result.innerHTML = textInput.value + ' is a palindrome'; 
   } else {
     result.innerHTML = textInput.value + ' is not a palindrome';
   }
 }
+
+checkBtn.addEventListener('click', palindrome);
